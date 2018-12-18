12d0a61425e14e83c1abda447dce6e9e80434293
Neo <neo@pop-os.localdomain>
2018-12-17 19:25:06 -0700

    Updates

---
+++ precache-manifest.dd9da2858237d006f2cf44b0520273d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c364cf488479538ee917",
+    "url": "/apiTables/static/js/runtime~main.c364cf48.js"
+  },
+  {
+    "revision": "9d6f9a207fcf37fa7f88",
+    "url": "/apiTables/static/js/main.9d6f9a20.chunk.js"
+  },
+  {
+    "revision": "2818dbc80ba3c939eefa",
+    "url": "/apiTables/static/js/1.2818dbc8.chunk.js"
+  },
+  {
+    "revision": "9d6f9a207fcf37fa7f88",
+    "url": "/apiTables/static/css/main.442499e5.chunk.css"
+  },
+  {
+    "revision": "096c20ac937881fbeee7b4d297155b70",
+    "url": "/apiTables/index.html"
+  }
+];
